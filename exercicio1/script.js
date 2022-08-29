6d11b0171e5e57f4025389193348f17847d41683
NataliaArruda87 <natalia.arruda87@gmail.com>
2022-08-29 14:50:16 -0300

    Soluçao exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,12 @@
+/*let nome
+let idade
+console.log(typeof nome, nome)
+console.log(typeof idade, idade)
+o tipo da variavel foi dado como undefined pois não houve atribuição de valores a variavel*/
+
+let nome = prompt("Qual o seu nome")
+let idade = prompt("Qual é a sua idade")
+console.log(typeof nome, nome)
+console.log(typeof idade, idade)
+//Ambas as variaveis são do tipo string
+console.log(`Olá ${nome}, você tem ${idade} anos`)
