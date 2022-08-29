b1bdf7e186bf9bca9553a542363a66f5c90a36d6
NataliaArruda87 <natalia.arruda87@gmail.com>
2022-08-29 15:39:11 -0300

    Solução exercicio 2 atualizada

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,9 @@
-const perguntaA = "Você tem cabelo castanho?"
-const corCabelo = confirm("Você tem cabelo castanho?")
+let perguntaA = "Você tem cabelo castanho?"
+let corCabelo = prompt(perguntaA)
 console.log(perguntaA, corCabelo)
-const perguntaB = "Você está usando chinelo?"
-const usaChinelo = confirm("Você está usando chinelo?")
+let perguntaB = "Você está usando chinelo?"
+let usaChinelo = prompt("Você está usando chinelo?")
 console.log(perguntaB, usaChinelo)
-const perguntaC = "A blusa que você está usando hoje é verde?"
-const corDaBlusa = confirm("A blusa que você está usando hoje é verde?")
+let perguntaC = "A blusa que você está usando hoje é verde?"
+let corDaBlusa = prompt("A blusa que você está usando hoje é verde?")
 console.log(perguntaC, corDaBlusa)
