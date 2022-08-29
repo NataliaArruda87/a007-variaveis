1a9a91ea6f5049b19d69b2176847497a2afb0645
NataliaArruda87 <natalia.arruda87@gmail.com>
2022-08-29 15:54:35 -0300

    Solução exercicio 3

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -2,8 +2,8 @@ let perguntaA = "Você tem cabelo castanho?"
 let corCabelo = prompt(perguntaA)
 console.log(perguntaA, corCabelo)
 let perguntaB = "Você está usando chinelo?"
-let usaChinelo = prompt("Você está usando chinelo?")
+let usaChinelo = prompt(perguntaB)
 console.log(perguntaB, usaChinelo)
 let perguntaC = "A blusa que você está usando hoje é verde?"
-let corDaBlusa = prompt("A blusa que você está usando hoje é verde?")
+let corDaBlusa = prompt(perguntaC)
 console.log(perguntaC, corDaBlusa)
