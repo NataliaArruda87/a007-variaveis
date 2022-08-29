8fd843fd21b6e32a00f561d4c9c92a3d903a13dd
NataliaArruda87 <natalia.arruda87@gmail.com>
2022-08-29 14:58:13 -0300

    Solução exercicio 2

---
+++ exercicio2/script.js
@@ -0,0 +1,9 @@
+const perguntaA = "Você tem cabelo castanho?"
+const corCabelo = confirm("Você tem cabelo castanho?")
+console.log(perguntaA, corCabelo)
+const perguntaB = "Você está usando chinelo?"
+const usaChinelo = confirm("Você está usando chinelo?")
+console.log(perguntaB, usaChinelo)
+const perguntaC = "A blusa que você está usando hoje é verde?"
+const corDaBlusa = confirm("A blusa que você está usando hoje é verde?")
+console.log(perguntaC, corDaBlusa)
